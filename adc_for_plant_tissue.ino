e19fc837b575d4a91cf918bcc37a2bedec3bf983
kenneth558 <kenlovesjesus@gmail.com>
2018-04-19 01:17:20 -0500

    Update adc_for_plant_tissue.ino

--- adc_for_plant_tissue.ino
+++ adc_for_plant_tissue.ino
@@ -2,16 +2,32 @@
 // ADC demo for Larduino w/ LGT8F328D
 // Using new added internal 2.56V reference
 //============================================
+
+#define NUM_ANALOG_INPUTS_TO_PLOT 1 //The number of consecutive analog pins to plot, beginning with PIN_A0
+
 #include <math.h>
+#include <Adafruit_ADS1015.h>//for systems using ADS1115/ADS1015
+
 long int value;//TheWeMos has 12-bit ADC resolution 
 const uint8_t ADC_RES_BIT = 12;
 const uint8_t SAMPLE_TIMES = 5;
-#define PIN_A0 14
-#define PIN_A1 15
-#define PIN_A2 16
-#define PIN_A3 17
-#define PIN_A4 18
-#define PIN_A5 19
+
+/*
+ * 
+ * The following pin defines are for the WeMos XI/TTGO XI board only
+ * Change per your board layout
+ * 
+ */
+#ifndef PIN_A0
+    #define PIN_A0 14
+    #define PIN_A1 15
+    #define PIN_A2 16
+    #define PIN_A3 17
+    #define PIN_A4 18
+    #define PIN_A5 19
+#endif
+
+uint8_t *A_PIN_ARRAY;
 
 void setup() 
 {
@@ -19,22 +35,141 @@ void setup()
     
     Serial.begin( 19200 );
     analogReadResolution( ADC_RES_BIT );
+    A_PIN_ARRAY = (uint8_t *)malloc( NUM_ANALOG_INPUTS );
+
+//Herafter is the pattern.  If you have more analog pins, add them according to the pattern.
+#ifdef NUM_ANALOG_INPUTS
+#ifdef PIN_A0
+    for( uint8_t i = 0; i < NUM_ANALOG_INPUTS_TO_PLOT; i++ )
+    {
+        if( i == 0 )
+            *( A_PIN_ARRAY + i ) = PIN_A0;
+#ifdef PIN_A1
+        else if( i == 1 )
+            *( A_PIN_ARRAY + i ) = PIN_A1;
+#ifdef PIN_A2
+        else if( i == 2 )
+            *( A_PIN_ARRAY + i ) = PIN_A2;
+#ifdef PIN_A3
+        else if( i == 3 )
+            *( A_PIN_ARRAY + i ) = PIN_A3;
+#ifdef PIN_A4
+        else if( i == 4 )
+            *( A_PIN_ARRAY + i ) = PIN_A4;
+#ifdef PIN_A5
+        else if( i == 5 )
+            *( A_PIN_ARRAY + i ) = PIN_A5;
+#ifdef PIN_A6
+        else if( i == 6 )
+            *( A_PIN_ARRAY + i ) = PIN_A6;
+#ifdef PIN_A7
+        else if( i == 7 )
+            *( A_PIN_ARRAY + i ) = PIN_A7;
+#ifdef PIN_A8
+        else if( i == 8 )
+            *( A_PIN_ARRAY + i ) = PIN_A8;
+#ifdef PIN_A9
+        else if( i == 9 )
+            *( A_PIN_ARRAY + i ) = PIN_A9;
+#ifdef PIN_A10
+        else if( i == 10 )
+            *( A_PIN_ARRAY + i ) = PIN_A10;
+#ifdef PIN_A11
+        else if( i == 11 )
+            *( A_PIN_ARRAY + i ) = PIN_A11;
+#ifdef PIN_A12
+        else if( i == 12 )
+            *( A_PIN_ARRAY + i ) = PIN_A12;
+#ifdef PIN_A13
+        else if( i == 13 )
+            *( A_PIN_ARRAY + i ) = PIN_A13;
+#ifdef PIN_A14
+        else if( i == 14 )
+            *( A_PIN_ARRAY + i ) = PIN_A14;
+#ifdef PIN_A15
+        else if( i == 15 )
+            *( A_PIN_ARRAY + i ) = PIN_A15;
+#ifdef PIN_A16
+        else if( i == 16 )
+            *( A_PIN_ARRAY + i ) = PIN_A16;
+#ifdef PIN_A17
+        else if( i == 17 )
+            *( A_PIN_ARRAY + i ) = PIN_A17;
+#ifdef PIN_A18
+        else if( i == 18 )
+            *( A_PIN_ARRAY + i ) = PIN_A18;
+#ifdef PIN_A19
+        else if( i == 19 )
+            *( A_PIN_ARRAY + i ) = PIN_A19;
+#ifdef PIN_A20
+        else if( i == 20 )
+            *( A_PIN_ARRAY + i ) = PIN_A20;
+#ifdef PIN_A21
+        else if( i == 21 )
+            *( A_PIN_ARRAY + i ) = PIN_A21;
+#ifdef PIN_A22
+        else if( i == 22 )
+            *( A_PIN_ARRAY + i ) = PIN_A22;
+#endif //end of PIN_A22 check
+#endif //end of PIN_A21 check
+#endif //end of PIN_A20 check
+#endif //end of PIN_A19 check
+#endif //end of PIN_A18 check
+#endif //end of PIN_A17 check
+#endif //end of PIN_A16 check
+#endif //end of PIN_A15 check
+#endif //end of PIN_A14 check
+#endif //end of PIN_A13 check
+#endif //end of PIN_A12 check
+#endif //end of PIN_A11 check
+#endif //end of PIN_A10 check
+#endif //end of PIN_A9 check
+#endif //end of PIN_A8 check
+#endif //end of PIN_A7 check
+#endif //end of PIN_A6 check
+#endif //end of PIN_A5 check
+#endif //end of PIN_A4 check
+#endif //end of PIN_A3 check
+#endif //end of PIN_A2 check
+#endif //end of PIN_A1 check
+    }
+#endif //end of PIN_A0 check
+#else
+    #define NUM_ANALOG_INPUTS 255
+#endif
 }
 
 void loop() 
 {
-    value = analogRead( PIN_A0 );
-    for( uint8_t sampletimes = 1; sampletimes < SAMPLE_TIMES; sampletimes++ )
+    if( NUM_ANALOG_INPUTS == 255 )
     {
-      value += analogRead( PIN_A0 );
+        bool beenhere = false;
+        if( !beenhere ) 
+        {
+            beenhere = true;
+            Serial.println( F( "Sorry, but you will have to manually define the variable NUM_ANALOG_INPUTS and re-compile..." ) );
+        }
     }
-    value = value / SAMPLE_TIMES;
-    Serial.print( value );
-    if( value < 1000 ) Serial.print( " " );
-    if( value < 100 ) Serial.print( " " );
-    if( value < 10 ) Serial.print( " " );
-    Serial.print( " at " );
-    Serial.print( ADC_RES_BIT );
-    Serial.println( " bits" );
-    delay( 500 );
+    else
+    {
+        for( uint8_t i = 0; i < NUM_ANALOG_INPUTS_TO_PLOT; i++ )
+        {
+            value = analogRead( *( A_PIN_ARRAY + i ) );
+            for( uint8_t sampletimes = 1; sampletimes < SAMPLE_TIMES; sampletimes++ )
+            {
+              value += analogRead( *( A_PIN_ARRAY + i ) );
+            }
+            value = value / SAMPLE_TIMES;
+            Serial.print( value );
+        //    if( value < 1000 ) Serial.print( " " );
+        //    if( value < 100 ) Serial.print( " " );
+        //    if( value < 10 ) 
+            Serial.print( " " );
+        //    Serial.print( " at " );
+        //    Serial.print( ADC_RES_BIT );
+        //    Serial.println( " bits" );
+        }
+        Serial.println();
+        delay( 100 );
+    }
 }
