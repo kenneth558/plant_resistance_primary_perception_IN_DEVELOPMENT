92af86a7c8507c98bd00c7d2088bc789d7cff3cd
kenneth558 <kenlovesjesus@gmail.com>
2018-04-29 15:44:42 -0500

    Update adc_for_plant_tissue.ino

--- adc_for_plant_tissue.ino
+++ adc_for_plant_tissue.ino
@@ -1,10 +1,13 @@
+
 // Before using this sketch, you must set the following appropriately for your configuration and preferences !!!
 #define NUM_ANALOG_INPUTS_TO_PLOT 1 //The number of consecutive analog pins to plot, beginning with PIN_A0
-#define NUM_ADS1X15_INPUTS_TO_PLOT 0 //The number of consecutive ADS1X15 pins to plot, beginning with A0
+#define NUM_ADS1X15_INPUTS_TO_PLOT 1 //The number of consecutive ADS1X15 pins to plot, beginning with A0
 #define MULTIPLICATION_FACTOR 15 //To aid in viewing
 #define HighestBitResFromADS 15 //This is ADS1115 single-ended, advertised res of 16 bit only applies to double-ended.  all ADC values will get scaled to this, change to 11 for ADS1015
 #define AnalogInputBitsOfBoard 10 //Most Arduino boards are 10-bit resolution 
 #define SAMPLE_TIMES 30 //To better average out artifacts we over-sample and average.  This value can be tweaked by you to ensure neutralization of power line noise or harmonics of power supplies, etc.....
+#define FIRST_ANALOG_PIN_DIGITAL_NUMBER_FOR_BOARDS_NOT_HAVING_ANALOG_PINS_DEFINED_BY_A0_TYPE_DEFINES 14
+//#define DEBUG //Don't forget that DEBUG is not formatted for Serial plotter so plotter can't work when you put the compiler in DEBUG
 
 /*******************(C)  COPYRIGHT 2018 KENNETH L ANDERSON *********************
 * 
@@ -49,26 +52,11 @@
 *  These planned enhancements will be reserved for NOT-FOR-FREE sketch versions in the future
 * 
 *********************************************************************************************************************/
-
 #define VERSION "v.Free"
-#if ( NUM_ANALOG_INPUTS_TO_PLOT > 0 )
-    #ifndef NUM_ANALOG_INPUTS
-Sorry, but you will have to manually define the variable NUM_ANALOG_INPUTS somewhere above this line and re-compile...
-    #endif
-#else
-    #if ( NUM_ADS1X15_INPUTS_TO_PLOT == 0 )
-You'll need to manually define at least one of the variables NUM_ANALOG_INPUTS_TO_PLOT or NUM_ADS1X15_INPUTS_TO_PLOT where they appear in the lines above and re-compile...
-If you only have the Arduino without an ADS1X15, then define NUM_ANALOG_INPUTS_TO_PLOT.  Otherwise, define NUM_ADS1X15_INPUTS_TO_PLOT and/or both of them.
-    #endif
-#endif
 
-
-//#define DEBUG //Don't forget that DEBUG is not formatted for Serial plotter so plotter can't work
 #include <math.h>
-
-
 #if ( NUM_ADS1X15_INPUTS_TO_PLOT > 0 )
-    #include "SPI.h"
+        #include "SPI.h"
 /*
 *
 *  SDA, SCL Wemos XI/TTGO XI are terribly mislabeled in slkscreen on the board!  Use A4 for SDA, and A5 is SCL.
@@ -84,8 +72,16 @@ This ADS resolution is not supported in this sketch version.
         #endif
     #endif
 #endif
-
-
+#if ( NUM_ANALOG_INPUTS_TO_PLOT > 0 )
+    #ifndef NUM_ANALOG_INPUTS
+Sorry, but you will have to manually define the variable NUM_ANALOG_INPUTS somewhere above this line and re-compile...
+    #endif
+#else
+    #if ( NUM_ADS1X15_INPUTS_TO_PLOT == 0 )
+You'll need to manually define at least one of the variables NUM_ANALOG_INPUTS_TO_PLOT or NUM_ADS1X15_INPUTS_TO_PLOT where they appear in the lines above and re-compile...
+If you only have the Arduino without an ADS1X15, then define NUM_ANALOG_INPUTS_TO_PLOT.  Otherwise, define NUM_ADS1X15_INPUTS_TO_PLOT and/or both of them.
+    #endif
+#endif
 
 /*
  * 
@@ -122,110 +118,6 @@ void setup()
 //#ifndef ARDUINO_AVR_DIGISPARKPRO
 //    analogReadResolution( ADC_RES_BIT );
 //#endif
-#if ( NUM_ANALOG_INPUTS_TO_PLOT > 0 )
-    A_PIN_ARRAY = (uint8_t *)malloc( NUM_ANALOG_INPUTS );
-
-//Herafter is the pattern.  If you have more analog pins, add them according to the pattern.
-#ifdef NUM_ANALOG_INPUTS
-#ifdef PIN_A0
-    for( uint8_t i = 0; i < NUM_ANALOG_INPUTS_TO_PLOT; i++ )
-    {
-        if( i == 0 )
-            *( A_PIN_ARRAY + i ) = PIN_A0;
-#ifdef PIN_A1
-        else if( i == 1 )
-            *( A_PIN_ARRAY + i ) = PIN_A1;
-#ifdef PIN_A2
-        else if( i == 2 )
-            *( A_PIN_ARRAY + i ) = PIN_A2;
-#ifdef PIN_A3
-        else if( i == 3 )
-            *( A_PIN_ARRAY + i ) = PIN_A3;
-#ifdef PIN_A4
-        else if( i == 4 )
-            *( A_PIN_ARRAY + i ) = PIN_A4;
-#ifdef PIN_A5
-        else if( i == 5 )
-            *( A_PIN_ARRAY + i ) = PIN_A5;
-#ifdef PIN_A6
-        else if( i == 6 )
-            *( A_PIN_ARRAY + i ) = PIN_A6;
-#ifdef PIN_A7
-        else if( i == 7 )
-            *( A_PIN_ARRAY + i ) = PIN_A7;
-#ifdef PIN_A8
-        else if( i == 8 )
-            *( A_PIN_ARRAY + i ) = PIN_A8;
-#ifdef PIN_A9
-        else if( i == 9 )
-            *( A_PIN_ARRAY + i ) = PIN_A9;
-#ifdef PIN_A10
-        else if( i == 10 )
-            *( A_PIN_ARRAY + i ) = PIN_A10;
-#ifdef PIN_A11
-        else if( i == 11 )
-            *( A_PIN_ARRAY + i ) = PIN_A11;
-#ifdef PIN_A12
-        else if( i == 12 )
-            *( A_PIN_ARRAY + i ) = PIN_A12;
-#ifdef PIN_A13
-        else if( i == 13 )
-            *( A_PIN_ARRAY + i ) = PIN_A13;
-#ifdef PIN_A14
-        else if( i == 14 )
-            *( A_PIN_ARRAY + i ) = PIN_A14;
-#ifdef PIN_A15
-        else if( i == 15 )
-            *( A_PIN_ARRAY + i ) = PIN_A15;
-#ifdef PIN_A16
-        else if( i == 16 )
-            *( A_PIN_ARRAY + i ) = PIN_A16;
-#ifdef PIN_A17
-        else if( i == 17 )
-            *( A_PIN_ARRAY + i ) = PIN_A17;
-#ifdef PIN_A18
-        else if( i == 18 )
-            *( A_PIN_ARRAY + i ) = PIN_A18;
-#ifdef PIN_A19
-        else if( i == 19 )
-            *( A_PIN_ARRAY + i ) = PIN_A19;
-#ifdef PIN_A20
-        else if( i == 20 )
-            *( A_PIN_ARRAY + i ) = PIN_A20;
-#ifdef PIN_A21
-        else if( i == 21 )
-            *( A_PIN_ARRAY + i ) = PIN_A21;
-#ifdef PIN_A22
-        else if( i == 22 )
-            *( A_PIN_ARRAY + i ) = PIN_A22;
-#endif //end of PIN_A22 check
-#endif //end of PIN_A21 check
-#endif //end of PIN_A20 check
-#endif //end of PIN_A19 check
-#endif //end of PIN_A18 check
-#endif //end of PIN_A17 check
-#endif //end of PIN_A16 check
-#endif //end of PIN_A15 check
-#endif //end of PIN_A14 check
-#endif //end of PIN_A13 check
-#endif //end of PIN_A12 check
-#endif //end of PIN_A11 check
-#endif //end of PIN_A10 check
-#endif //end of PIN_A9 check
-#endif //end of PIN_A8 check
-#endif //end of PIN_A7 check
-#endif //end of PIN_A6 check
-#endif //end of PIN_A5 check
-#endif //end of PIN_A4 check
-#endif //end of PIN_A3 check
-#endif //end of PIN_A2 check
-#endif //end of PIN_A1 check
-    }
-#endif //end of PIN_A0 check
-#else
-    #define NUM_ANALOG_INPUTS 255
-#endif
-#endif
 #if ( NUM_ADS1X15_INPUTS_TO_PLOT > 0 )
 //   ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
    ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV  //This allows a simple power rail excitation supply to voltage divider
@@ -261,22 +153,134 @@ void setup()
         Serial.print( 0 );
         Serial.print( F( " " ) );
     }
+//    Serial.println( F( "0" ) );
     Serial.println( PlotterMaxScale );
+#if ( NUM_ANALOG_INPUTS_TO_PLOT > 0 )
+    uint8_t *A_PIN_ARRAY = (uint8_t *)malloc( NUM_ANALOG_INPUTS );
+
+//Herafter is the pattern.  If you have more analog pins, add them according to the pattern.
+#ifdef NUM_ANALOG_INPUTS
+#ifdef A0
+    for( uint8_t i = 0; i < NUM_ANALOG_INPUTS_TO_PLOT; i++ )
+    {
+        if( i == 0 )
+            A_PIN_ARRAY[ i ] = A0;
 #ifdef DEBUG
-        Serial.println( F( " end of setup" ) );
+    Serial.println( A_PIN_ARRAY[ i ] );
+#endif
+#ifdef A1
+        else if( i == 1 )
+            A_PIN_ARRAY[ i ] = A1;
+#ifdef A2
+        else if( i == 2 )
+            A_PIN_ARRAY[ i ] = A2;
+#ifdef PIN_A3
+        else if( i == 3 )
+            A_PIN_ARRAY[ i ] = A3;
+#ifdef PIN_A4
+        else if( i == 4 )
+            A_PIN_ARRAY[ i ] = A4;
+#ifdef PIN_A5
+        else if( i == 5 )
+            A_PIN_ARRAY[ i ] = A5;
+#ifdef PIN_A6
+        else if( i == 6 )
+            A_PIN_ARRAY[ i ] = A6;
+#ifdef PIN_A7
+        else if( i == 7 )
+            A_PIN_ARRAY[ i ] = A7;
+#ifdef PIN_A8
+        else if( i == 8 )
+            A_PIN_ARRAY[ i ] = A8;
+#ifdef PIN_A9
+        else if( i == 9 )
+            A_PIN_ARRAY[ i ] = A9;
+#ifdef PIN_A10
+        else if( i == 10 )
+            A_PIN_ARRAY[ i ] = A10;
+#ifdef PIN_A11
+        else if( i == 11 )
+            A_PIN_ARRAY[ i ] = A11;
+#ifdef PIN_A12
+        else if( i == 12 )
+            A_PIN_ARRAY[ i ] = A12;
+#ifdef PIN_A13
+        else if( i == 13 )
+            A_PIN_ARRAY[ i ] = A13;
+#ifdef PIN_A14
+        else if( i == 14 )
+            A_PIN_ARRAY[ i ] = A14;
+#ifdef PIN_A15
+        else if( i == 15 )
+            A_PIN_ARRAY[ i ] = A15;
+#ifdef PIN_A16
+        else if( i == 16 )
+            A_PIN_ARRAY[ i ] = A16;
+#ifdef PIN_A17
+        else if( i == 17 )
+            A_PIN_ARRAY[ i ] = A17;
+#ifdef PIN_A18
+        else if( i == 18 )
+            A_PIN_ARRAY[ i ] = A18;
+#ifdef PIN_A19
+        else if( i == 19 )
+            A_PIN_ARRAY[ i ] = A19;
+#ifdef PIN_A20
+        else if( i == 20 )
+            A_PIN_ARRAY[ i ] = A20;
+#ifdef PIN_A21
+        else if( i == 21 )
+            A_PIN_ARRAY[ i ] = A21;
+#ifdef PIN_A22
+        else if( i == 22 )
+            A_PIN_ARRAY[ i ] = A22;
+#endif //end of A22 check
+#endif //end of A21 check
+#endif //end of A20 check
+#endif //end of A19 check
+#endif //end of A18 check
+#endif //end of A17 check
+#endif //end of A16 check
+#endif //end of A15 check
+#endif //end of A14 check
+#endif //end of A13 check
+#endif //end of A12 check
+#endif //end of A11 check
+#endif //end of A10 check
+#endif //end of A9 check
+#endif //end of A8 check
+#endif //end of A7 check
+#endif //end of A6 check
+#endif //end of A5 check
+#endif //end of A4 check
+#endif //end of A3 check
+#endif //end of A2 check
+#endif //end of A1 check
+    }
+#else
+    #ifdef DEBUG  //This is to demonstrate the purpose of DEBUG
+            Serial.println( F( "Creating analog pin definitions" ) );
+    #endif
+    for( uint8_t i = 0; i < NUM_ANALOG_INPUTS_TO_PLOT; i++ )
+        A_PIN_ARRAY[ i ] = i + FIRST_ANALOG_PIN_DIGITAL_NUMBER_FOR_BOARDS_NOT_HAVING_ANALOG_PINS_DEFINED_BY_A0_TYPE_DEFINES;
+#endif //end of A0 check
+#endif
+#endif
+#ifdef DEBUG  //This is to demonstrate the purpose of DEBUG
+#ifdef A0
+        Serial.println( F( "Using native analog pin definitions" ) );
+#else
+        Serial.print( F( "No native analog pin definitions to use, Pin A0 should be " ) );
+        Serial.println( FIRST_ANALOG_PIN_DIGITAL_NUMBER_FOR_BOARDS_NOT_HAVING_ANALOG_PINS_DEFINED_BY_A0_TYPE_DEFINES );
+#endif
+        for( uint8_t i = 0; i < NUM_ANALOG_INPUTS_TO_PLOT; i++ )
+            Serial.println( A_PIN_ARRAY[ i ] );
+        Serial.println( F( "End of setup" ) );
 #endif
 }
 
-void plot_the_normal_and_magnified_signals( uint8_t i)
+void plot_the_normal_and_magnified_signals( uint8_t i )
 {
-#ifdef DEBUG
-    Serial.print( F( "Line273 i=" ) );
-    Serial.print( i );
-    Serial.print( F( ", high_limit_of_this_plotline=" ) );
-    Serial.print( screen_offsets[ i ].high_limit_of_this_plotline );
-    Serial.print( F( ", zero_of_this_plotline=" ) );
-    Serial.println( screen_offsets[ i ].zero_of_this_plotline );
-#endif
     value = ( uint16_t )( value / SAMPLE_TIMES );
     Serial.print( value + screen_offsets[ i ].zero_of_this_plotline ); //This is color one
     
@@ -284,72 +288,42 @@ void plot_the_normal_and_magnified_signals( uint8_t i)
     Serial.print( F( " " ) );
     
     value *= MULTIPLICATION_FACTOR;
-    
-#ifdef DEBUG
-    Serial.println( F( "Line289" ) );
-#endif
-    //if screen offset is so great that value will plot lower than plotline zero, raise it by amount such that when subtracted from value the result is ( high_limit_of_this_plotline - zero_of_this_plotline ) * .75
+
     if( screen_offsets[ i ].magnify_adjustment + screen_offsets[ i ].zero_of_this_plotline > value )
-        screen_offsets[ i ].magnify_adjustment = value - screen_offsets[ i ].zero_of_this_plotline;
-#ifdef DEBUG
-    Serial.print( F( "value - screen_offsets[ i ].magnify_adjustment=" ) );Serial.println( value - screen_offsets[ i ].magnify_adjustment );
-#endif
-    while( value - screen_offsets[ i ].magnify_adjustment > screen_offsets[ i ].high_limit_of_this_plotline - ( unsigned long )( ( screen_offsets[ i ].high_limit_of_this_plotline - screen_offsets[ i ].zero_of_this_plotline ) * ( unsigned long ).5 ) ) screen_offsets[ i ].magnify_adjustment += HundredthPlotterMaxScale; // lower too-high plot lines
-#ifdef DEBUG
-    Serial.print( F( "value - screen_offsets[ i ].magnify_adjustment=" ) );Serial.println( value - screen_offsets[ i ].magnify_adjustment );
-#endif
-#ifdef DEBUG
-    Serial.println( F( "Line302" ) );
-#endif
-    //Plot it now
+        screen_offsets[ i ].magnify_adjustment = value - screen_offsets[ i ].zero_of_this_plotline - ( ( screen_offsets[ i ].high_limit_of_this_plotline - screen_offsets[ i ].zero_of_this_plotline ) *.1 ) ;
+
+    if( screen_offsets[ i ].magnify_adjustment + screen_offsets[ i ].high_limit_of_this_plotline < value )
+        screen_offsets[ i ].magnify_adjustment = value - screen_offsets[ i ].high_limit_of_this_plotline + ( ( screen_offsets[ i ].high_limit_of_this_plotline - screen_offsets[ i ].zero_of_this_plotline ) *.1 ) ;
+        
+        //Plot it now
     Serial.print( value - screen_offsets[ i ].magnify_adjustment ); //This is color two or four
     Serial.print( F( " " ) );
 }
 
 void loop() 
 {    
-#ifdef DEBUG
-        Serial.print( F( "Line312" ) );
-#endif
     for( uint16_t plotter_loops = 0; plotter_loops < 500 / 3; plotter_loops++ ) 
     {
 #if ( NUM_ANALOG_INPUTS_TO_PLOT > 0 ) //plot the inboard analogs first and above
         for( uint8_t i = 0; i < NUM_ANALOG_INPUTS_TO_PLOT; i++ )
         {            
-            value = analogRead( *( A_PIN_ARRAY + i ) ) << BitsToShiftInboardADCValues;
+            value = analogRead( 14 ) << BitsToShiftInboardADCValues;
+//            value = analogRead( *( A_PIN_ARRAY + i ) ) << BitsToShiftInboardADCValues;
 
             for( uint8_t sampletimes = 1; sampletimes < SAMPLE_TIMES; sampletimes++ )
-                  value += ( analogRead( *( A_PIN_ARRAY + i ) ) << BitsToShiftInboardADCValues );
-
-#ifdef DEBUG
-        Serial.println( F( "Line325" ) );
-#endif
+                  value += ( analogRead( 14 ) << BitsToShiftInboardADCValues );
+//                  value += ( analogRead( *( A_PIN_ARRAY + i ) ) << BitsToShiftInboardADCValues );
             plot_the_normal_and_magnified_signals( i );
-#ifdef DEBUG
-        Serial.println( F( "Line329" ) );
-#endif
         }
 #endif
 
 #if ( NUM_ADS1X15_INPUTS_TO_PLOT > 0 )
         for( uint8_t i = 0; i < NUM_ADS1X15_INPUTS_TO_PLOT; i++ )
         {
-#ifdef DEBUG
-        Serial.print( F( "Line338 i=" ) );
-        Serial.println( i );
-#endif
             value = ads.readADC_SingleEnded( i );
-#ifdef DEBUG
-        Serial.print( F( "Line343 value=" ) );
-        Serial.println( value );
-#endif
             while( value > pow( 2, HighestBitResFromADS ) )
             {
                 value = ads.readADC_SingleEnded( i );
-#ifdef DEBUG
-        Serial.print( F( "Line350 value=" ) );
-        Serial.println( value );
-#endif
             }
             for( uint8_t sampletimes = 1; sampletimes < SAMPLE_TIMES; sampletimes++ )
             {
@@ -357,17 +331,7 @@ void loop()
                 while( valueTemp > pow( 2, HighestBitResFromADS ) ) valueTemp = ads.readADC_SingleEnded( i );
                 value += valueTemp;
             }
-#ifdef DEBUG
-        Serial.println( F( "Line361" ) );
-#endif
-
-#ifdef DEBUG
-        Serial.println( F( "Line365" ) );
-#endif
             plot_the_normal_and_magnified_signals( i + NUM_ANALOG_INPUTS_TO_PLOT );
-#ifdef DEBUG
-        Serial.println( F( "Line369" ) );
-#endif
         }
 #endif
         if( graphline ) valueTemp = 0;
